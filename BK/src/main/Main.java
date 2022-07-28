1cc4562fe86cb96d43298a7d1d611318514b9259
DESKTOP-3KKTEV5\USER <qhrud5991@gmail.com>
2022-07-28 17:27:09 +0900

    김보경-기부하기,이체하기 1자착업(22.07.08)

---
+++ BK/src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+import control.MemberDonation;
+import control.MemberSendMoney;
+import table.EcoMemberDto;
+import utill.InputUtil;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//기부하기
+		//MemberDonation d = new MemberDonation();
+		//d.memberDonation();
+		
+		//이체하기
+		MemberSendMoney s = new MemberSendMoney();
+		s.memberSendMoney();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}// main
+	
+}// class
